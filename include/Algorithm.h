b0242c67be57af8ad0bae4c70c84c2575d9b34e4
Mrokkk <mrokk@protonmail.com>
2016-06-17 17:39:24 +0200

    Add for_each, count and count_if functions

---
+++ include/Algorithm.h
@@ -0,0 +1,40 @@
+//
+// Created by maciek on 15.06.16.
+//
+
+#pragma once
+
+#include <Move.h>
+
+template <typename Container, typename Lambda>
+inline Lambda &&for_each(Container &c, Lambda lambda)
+{
+    for (auto &it : c)
+        lambda(it);
+    return move(lambda);
+}
+
+template <typename Iterator, typename Lambda>
+inline Lambda &&for_each(Iterator first, Iterator last, Lambda lambda)
+{
+    for (; first != last; ++first)
+        lambda(*first);
+    return move(lambda);
+}
+
+template <typename Container, typename Type>
+inline int count(Container &c, Type elem)
+{
+    auto result = 0;
+    for_each(c, [ & ](const Type &e) { if (e == elem) result++; });
+    return result;
+};
+
+template <typename Container, typename Lambda>
+inline int count_if(Container &c, Lambda lambda)
+{
+    auto result = 0;
+    for_each(c, [ & ](const auto &e) { if (lambda(e)) result++; });
+    return result;
+};
+
--- tests/Mixed.cpp
+++ tests/Mixed.cpp
@@ -5,6 +5,7 @@
 #include <boost/test/unit_test.hpp>
 #include <List.h>
 #include <Pointer.h>
+#include <Algorithm.h>
 
 BOOST_AUTO_TEST_SUITE(MixedTests)
 
@@ -18,6 +19,37 @@ BOOST_AUTO_TEST_SUITE(MixedTests)
         BOOST_CHECK_EQUAL(**it, 6);
         it++;
         BOOST_CHECK_EQUAL(**it, 5);
+    }
+
+    BOOST_AUTO_TEST_CASE(canUseForEachOnList)
+    {
+        List<int> list;
+        int result[3];
+        list.push_front(3);
+        list.push_front(5);
+        list.push_front(78);
+        int i = 0;
+        for_each(list, [ & ](int &e) { result[i++] = e; });
+        BOOST_CHECK_EQUAL(result[0], 78);
+        BOOST_CHECK_EQUAL(result[1], 5);
+        BOOST_CHECK_EQUAL(result[2], 3);
+        BOOST_CHECK_EQUAL(count(list, 78), 1);
+        for_each(list.begin(), list.end(), [ ](int &e) { e = 22; });
+        i = 0;
+        for_each(list, [ & ](int &e) { result[i++] = e; });
+        BOOST_CHECK_EQUAL(result[0], 22);
+        BOOST_CHECK_EQUAL(result[1], 22);
+        BOOST_CHECK_EQUAL(result[2], 22);
+    }
+
+    BOOST_AUTO_TEST_CASE(canUseCountOnList)
+    {
+        List<int> list{3, 7, 8, 6, 5, 3, 7, 9, 60, 55, 8, 6};
+        BOOST_CHECK_EQUAL(count(list, 3), 2);
+        BOOST_CHECK_EQUAL(count(list, 9), 1);
+        BOOST_CHECK_EQUAL(count(list, 20), 0);
+        BOOST_CHECK_EQUAL(count_if(list, [](const int &e){ return e > 3; }), 10);
+        BOOST_CHECK_EQUAL(count_if(list, [](const int &e){ return e < 3; }), 0);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
