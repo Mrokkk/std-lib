53f0d07efb0543b5208a799face2ec519fd42e3a
Mrokkk <mrokk@protonmail.com>
2016-06-18 19:48:34 +0200

    Refactor the initializer_list

--- include/initializer_list.h
+++ include/initializer_list.h
@@ -6,35 +6,37 @@
 
 namespace std
 {
-template<typename __T>
-class initializer_list
-{
-    const __T *__array;
-    unsigned int __size;
-
-    initializer_list(const __T *__a, unsigned int __l)
-        : __array(__a), __size(__l)
-    { }
-
-public:
-    initializer_list()
-        : __array(0), __size(0)
-    { }
-
-    unsigned int size() const
+    template <typename Type>
+    class initializer_list
     {
-        return __size;
-    }
 
-    const __T *begin() const
-    {
-        return __array;
-    }
+        const Type *_array;
+        unsigned int _size;
 
-    const __T *end() const
-    {
-        return  &__array[__size];
-    }
+        initializer_list(const Type *array, unsigned int length)
+            : _array(array), _size(length)
+        { }
 
-};
+    public:
+
+        initializer_list()
+            : _array(0), _size(0)
+        { }
+
+        unsigned int size() const
+        {
+            return _size;
+        }
+
+        const Type *begin() const
+        {
+            return _array;
+        }
+
+        const Type *end() const
+        {
+            return &_array[_size];
+        }
+
+    };
 }
