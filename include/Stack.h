58157a9850ade86653d623f91a4a533cbb17d172
Mrokkk <mrokk@protonmail.com>
2016-07-10 13:21:04 +0200

    Improve stack implementation

--- include/Stack.h
+++ include/Stack.h
@@ -15,11 +15,6 @@ private:
 
 public:
 
-    inline void push(ElementType &element)
-    {
-        _list.push_back(element);
-    }
-
     inline void push(const ElementType &element)
     {
         _list.push_back(element);
@@ -30,23 +25,17 @@ public:
         _list.pop_back();
     }
 
-    inline ElementType front()
+    inline const ElementType &front() const
     {
         return _list.back();
     }
 
-    inline unsigned long size()
+    inline unsigned long size() const
     {
         return _list.size();
     }
 
     inline Stack &operator <<(const ElementType &element)
-    {
-        push(element);
-        return *this;
-    }
-
-    inline Stack &operator <<(ElementType &element)
     {
         push(element);
         return *this;
