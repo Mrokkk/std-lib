39d9f044d8abac33977ebe2c78478e4fe74a4ac9
Maciek <mrokk@protonmail.com>
2016-08-18 21:01:03 +0200

    Change ci.sh

--- ci.sh
+++ ci.sh
@@ -1,10 +1,14 @@
 mkdir -p build
-pushd build
-cmake ..
-make
-popd
-if [[ $VALGRIND ]]; then
-    valgrind ./bin/std_lib
-else
-    ./bin/std_lib
-fi
+cd build
+case $1 in
+    build )
+        cmake ..
+        make ;;
+    run_tests )
+        if [[ $VALGRIND ]]; then
+            valgrind ../bin/std_lib
+        else
+            ../bin/std_lib
+        fi ;;
+esac
+
