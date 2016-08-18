2e9c2ada76e6b10c81ba555943e312e1d9c726a9
Maciek <mrokk@protonmail.com>
2016-08-18 22:33:34 +0200

    Add creating code coverage report

--- ci.sh
+++ ci.sh
@@ -1,14 +1,20 @@
 mkdir -p build
 cd build
 case $1 in
-    build )
-        cmake ..
-        make ;;
-    run_tests )
+    ut )
+        if [[ $COVERAGE ]]; then
+            cmake .. -DCOVERAGE=ON
+        else
+            cmake ..
+        fi
+        make
         if [[ $VALGRIND ]]; then
             valgrind ../bin/std_lib
         else
             ../bin/std_lib
+        fi
+        if [[ $COVERAGE ]]; then
+            make coverage
         fi ;;
 esac
 
