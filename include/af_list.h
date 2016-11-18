c3ff05291d1ee21b64502a954fa42bdc9a1df0cf
Mrokkk <mrokk@protonmail.com>
2016-11-18 19:34:04 +0100

    Fix af_list

--- include/af_list.h
+++ include/af_list.h
@@ -7,6 +7,7 @@ namespace yacppl {
 namespace af_list {
 
 struct list_head {
+
     list_head *next = this, *prev = this;
 
     bool empty() {
@@ -17,40 +18,40 @@ struct list_head {
 
 using list_element = list_head;
 
-void list_init(list_head *list) {
+inline void list_init(list_head *list) {
     list->next = list->prev = list;
 }
 
 namespace detail {
 
-void __list_add(list_head *new_element, list_head *prev, list_head *next) {
+inline void __list_add(list_head *new_element, list_head *prev, list_head *next) {
     next->prev = new_element;
     prev->next = new_element;
     new_element->next = next;
     new_element->prev = prev;
 }
 
-void __list_del(list_head *prev, list_head *next) {
+inline void __list_del(list_head *prev, list_head *next) {
     next->prev = prev;
     prev->next = next;
 }
 
 } // namespace detail
 
-void list_del(list_head *entry) {
+inline void list_del(list_head *entry) {
     detail::__list_del(entry->prev, entry->next);
     entry->next = entry;
     entry->prev = entry;
 }
 
-void list_add(list_head *new_element, list_head *head) {
+inline void list_add(list_head *new_element, list_head *head) {
     detail::__list_add(new_element, head, head->next);
 }
 
-void list_add_tail(list_head *new_element, list_head *head) {
+inline void list_add_tail(list_head *new_element, list_head *head) {
     detail::__list_add(new_element, head->prev, head);
 }
-void list_move(list_head *list, list_head *head) {
+inline void list_move(list_head *list, list_head *head) {
     detail::__list_del(list->prev, list->next);
     list_add_tail(list, head);
 }
