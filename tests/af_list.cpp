f797ef75016d6902a885b6a98d8290983d686c22
Mrokkk <mrokk@protonmail.com>
2016-11-19 21:10:54 +0100

    af_list.cpp: refactoring

--- tests/af_list.cpp
+++ tests/af_list.cpp
@@ -3,13 +3,25 @@
 
 using namespace yacppl;
 
-
+namespace {
 struct helper {
     int a;
     af_list::list_head list;
     helper(int x) : a(x) {}
 };
 
+void addTest(af_list::list_head &head, helper &e, std::vector<int> &comp, int s) {
+    auto size = 0;
+    list_add_tail(&e.list, &head);
+    af_list::list_for_each_entry(&head, &helper::list, [&] (helper *h) {
+        EXPECT_EQ(comp[size], h->a);
+        size++;
+    });
+    EXPECT_EQ(size, s);
+    EXPECT_EQ(head.empty(), false);
+}
+
+} // namespace anon
 
 TEST(AFListTests, canCreateEmpty) {
     af_list::list_head list;
@@ -20,31 +32,12 @@ TEST(AFListTests, canAddElements) {
     af_list::list_head testList;
     helper e1(2), e2(44), e3(26), e4(94);
     std::vector<int> v{2, 44, 26, 94};
-    list_add_tail(&e1.list, &testList);
-    auto size = 0;
-    af_list::list_for_each_entry(&testList, &helper::list, [&] (helper *h) {
-        EXPECT_EQ(v[size], h->a);
-        size++;
-    });
-    EXPECT_EQ(size, 1);
-    EXPECT_EQ(testList.empty(), false);
+    addTest(testList, e1, v, 1);
+    addTest(testList, e2, v, 2);
+    addTest(testList, e3, v, 3);
+    addTest(testList, e4, v, 4);
+}
 
-    size = 0;
-    list_add_tail(&e2.list, &testList);
-    af_list::list_for_each_entry(&testList, &helper::list, [&] (helper *h) {
-        EXPECT_EQ(v[size], h->a);
-        size++;
-    });
-    EXPECT_EQ(size, 2);
-    EXPECT_EQ(testList.empty(), false);
-
-    size = 0;
-    list_add_tail(&e3.list, &testList);
-    af_list::list_for_each_entry(&testList, &helper::list, [&] (helper *h) {
-        EXPECT_EQ(v[size], h->a);
-        size++;
-    });
-    EXPECT_EQ(size, 3);
-    EXPECT_EQ(testList.empty(), false);
+TEST(AFListTests, canDeleteElements) {
 }
 
