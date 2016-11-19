64d9f1acf0a7835858f0145b12570f4d12ad9eca
Mrokkk <mrokk@protonmail.com>
2016-11-19 21:20:51 +0100

    Add test

--- tests/af_list.cpp
+++ tests/af_list.cpp
@@ -21,6 +21,17 @@ void addTest(af_list::list_head &head, helper &e, std::vector<int> &comp, int s)
     EXPECT_EQ(head.empty(), false);
 }
 
+void delTest(af_list::list_head &head, helper &e, std::vector<int> &comp) {
+    af_list::list_del(&e.list);
+    size_t size = 0;
+    af_list::list_for_each_entry(&head, &helper::list, [&] (helper *h) {
+        EXPECT_EQ(comp[size], h->a);
+        size++;
+    });
+    EXPECT_EQ(size, comp.size());
+    EXPECT_EQ(head.empty(), false);
+}
+
 } // namespace anon
 
 TEST(AFListTests, canCreateEmpty) {
@@ -29,15 +40,26 @@ TEST(AFListTests, canCreateEmpty) {
 }
 
 TEST(AFListTests, canAddElements) {
-    af_list::list_head testList;
+    af_list::list_head list;
     helper e1(2), e2(44), e3(26), e4(94);
     std::vector<int> v{2, 44, 26, 94};
-    addTest(testList, e1, v, 1);
-    addTest(testList, e2, v, 2);
-    addTest(testList, e3, v, 3);
-    addTest(testList, e4, v, 4);
+    addTest(list, e1, v, 1);
+    addTest(list, e2, v, 2);
+    addTest(list, e3, v, 3);
+    addTest(list, e4, v, 4);
 }
 
 TEST(AFListTests, canDeleteElements) {
+    af_list::list_head list;
+    helper e1(2), e2(44), e3(26), e4(94);
+    std::vector<int> v{2, 44, 26, 94};
+    addTest(list, e1, v, 1);
+    addTest(list, e2, v, 2);
+    addTest(list, e3, v, 3);
+    addTest(list, e4, v, 4);
+    std::vector<int> v2{2, 44, 94};
+    delTest(list, e3, v2);
+    std::vector<int> v3{44, 94};
+    delTest(list, e1, v3);
 }
 
