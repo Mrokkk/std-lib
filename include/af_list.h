7e207ec1dce3419d95bc9946410afcd51d6466da
Mrokkk <mrokk@protonmail.com>
2016-11-22 09:07:32 +0100

    Remove old af_list implementation, fix tests

--- include/af_list.h
+++ include/af_list.h
@@ -6,18 +6,6 @@
 
 namespace yacppl {
 
-namespace af_list {
-
-struct list_head {
-
-    list_head *next = this, *prev = this;
-
-    bool empty() const {
-        return (prev == this) && (next == this);
-    }
-
-};
-
 template <class Type>
 class af_list {
 
@@ -33,12 +21,21 @@ class af_list {
         prev->next = next;
     }
 
+    template<typename T, typename U>
+    constexpr size_t offset_of(U T::*member) {
+        return (char*)&((T*)nullptr->*member) - (char*)nullptr;
+    }
+
 public:
 
     af_list *next = this, *prev = this;
 
     void add(af_list *new_element) {
         __list_add(new_element, prev, this);
+    }
+
+    void add_front(af_list *new_element) {
+        __list_add(new_element, this, next);
     }
 
     void del() {
@@ -65,72 +62,15 @@ public:
 
 };
 
-using list_element = list_head;
-
-inline void list_init(list_head *list) {
-    list->next = list->prev = list;
-}
-
-namespace detail {
-
-inline void __list_add(list_head *new_element, list_head *prev, list_head *next) {
-    next->prev = new_element;
-    prev->next = new_element;
-    new_element->next = next;
-    new_element->prev = prev;
-}
-
-inline void __list_del(list_head *prev, list_head *next) {
-    next->prev = prev;
-    prev->next = next;
-}
-
-} // namespace detail
-
-inline void list_del(list_head *entry) {
-    detail::__list_del(entry->prev, entry->next);
-    entry->next = entry;
-    entry->prev = entry;
-}
-
-inline void list_add(list_head *new_element, list_head *head) {
-    detail::__list_add(new_element, head, head->next);
-}
-
-inline void list_add_tail(list_head *new_element, list_head *head) {
-    detail::__list_add(new_element, head->prev, head);
-}
-
+#if 0
 inline void list_move(list_head *list, list_head *head) {
     detail::__list_del(list->prev, list->next);
     list_add_tail(list, head);
 }
 
-template <class Type>
-auto list_entry(list_head *head, size_t Offset) {
-    return reinterpret_cast<Type *>(reinterpret_cast<char *>(head) - reinterpret_cast<unsigned long>((reinterpret_cast<Type *>(Offset))));
-}
-
-template<typename T, typename U>
-constexpr size_t offset_of(U T::*member) {
-    return (char*)&((T*)nullptr->*member) - (char*)nullptr;
-}
-
-template <class Type, class Member, typename Func>
-void list_for_each_entry(yacppl::af_list::list_head *head, Member Type::* m, Func lambda) {
-    auto offset = offset_of(m);
-    for (auto it = list_entry<Type>(head->next, offset); &(it->*m) != head; it = list_entry<Type>(reinterpret_cast<list_head *>(reinterpret_cast<char *>(it) + offset)->next, offset)) {
-        lambda(it);
-    }
-}
-
 #define list_next_entry(ptr, type, member) \
     (reinterpret_cast<type *>(reinterpret_cast<char *>((ptr)->next)-reinterpret_cast<unsigned long>(&(reinterpret_cast<type *>(0))->member)))
-
-#define list_for_each(pos, head) \
-    for (pos = (head)->next; pos != (head); pos = pos->next)
-
-} // namespace af_list
+#endif
 
 } // namespace yacppl
 
--- tests/af_list.cpp
+++ tests/af_list.cpp
@@ -4,33 +4,17 @@
 using namespace yacppl;
 
 namespace {
+
 struct helper {
     int a;
-    af_list::list_head list;
+    af_list<helper> list;
     helper(int x) : a(x) {}
 };
 
-struct helper2 {
-    int a;
-    af_list::af_list<helper2> list;
-    helper2(int x) : a(x) {}
-};
-
-void addTest(af_list::list_head &head, helper &e, std::vector<int> &comp, int s) {
-    auto size = 0;
-    list_add_tail(&e.list, &head);
-    af_list::list_for_each_entry(&head, &helper::list, [&] (helper *h) {
-        EXPECT_EQ(comp[size], h->a);
-        size++;
-    });
-    EXPECT_EQ(size, s);
-    EXPECT_EQ(head.empty(), false);
-}
-
-void addTest2(af_list::af_list<helper2> &head, helper2 &e, std::vector<int> &comp, int s) {
+void testAdding(af_list<helper> &head, helper &e, std::vector<int> &comp, int s) {
     auto size = 0;
     head.add(&e.list);
-    head.for_each_entry(&helper2::list, [&] (helper2 *h) {
+    head.for_each_entry(&helper::list, [&] (helper *h) {
         EXPECT_EQ(comp[size], h->a);
         size++;
     });
@@ -38,21 +22,21 @@ void addTest2(af_list::af_list<helper2> &head, helper2 &e, std::vector<int> &com
     EXPECT_EQ(head.empty(), false);
 }
 
-void delTest(af_list::list_head &head, helper &e, std::vector<int> &comp) {
-    af_list::list_del(&e.list);
-    size_t size = 0;
-    af_list::list_for_each_entry(&head, &helper::list, [&] (helper *h) {
+void testAddingFront(af_list<helper> &head, helper &e, std::vector<int> &comp, int s) {
+    auto size = 0;
+    head.add_front(&e.list);
+    head.for_each_entry(&helper::list, [&] (helper *h) {
         EXPECT_EQ(comp[size], h->a);
         size++;
     });
-    EXPECT_EQ(size, comp.size());
+    EXPECT_EQ(size, s);
     EXPECT_EQ(head.empty(), false);
 }
 
-void delTest2(af_list::af_list<helper2> &head, helper2 &e, std::vector<int> &comp) {
+void testDeleting(af_list<helper> &head, helper &e, std::vector<int> &comp) {
     e.list.del();
     size_t size = 0;
-    head.for_each_entry(&helper2::list, [&] (helper2 *h) {
+    head.for_each_entry(&helper::list, [&] (helper *h) {
         EXPECT_EQ(comp[size], h->a);
         size++;
     });
@@ -63,71 +47,41 @@ void delTest2(af_list::af_list<helper2> &head, helper2 &e, std::vector<int> &com
 } // namespace anon
 
 TEST(AFListTests, canCreateEmpty) {
-    af_list::list_head list;
+    af_list<helper> list;
     EXPECT_EQ(list.empty(), true);
 }
 
 TEST(AFListTests, canAccesElement) {
     for (auto i = 0; i < 1024; i++) {
         helper e(i);
-        auto result = af_list::list_entry<helper>(&e.list, offsetof(helper, list));
-        EXPECT_EQ(result->a, i);
-    }
-}
-
-TEST(AFListTests2, canAccesElement) {
-    for (auto i = 0; i < 1024; i++) {
-        helper2 e(i);
         auto result = e.list.entry(offsetof(helper, list));
         EXPECT_EQ(result->a, i);
     }
 }
 
 TEST(AFListTests, canAddElements) {
-    af_list::list_head list;
-    helper e1(2), e2(44), e3(26), e4(94);
+    af_list<helper> list;
+    helper e1(2), e2(44), e3(26), e4(94), e5(24);
     std::vector<int> v{2, 44, 26, 94};
-    addTest(list, e1, v, 1);
-    addTest(list, e2, v, 2);
-    addTest(list, e3, v, 3);
-    addTest(list, e4, v, 4);
-}
-
-TEST(AFListTests2, canAddElements) {
-    af_list::af_list<helper2> list;
-    helper2 e1(2), e2(44), e3(26), e4(94);
-    std::vector<int> v{2, 44, 26, 94};
-    addTest2(list, e1, v, 1);
-    addTest2(list, e2, v, 2);
-    addTest2(list, e3, v, 3);
-    addTest2(list, e4, v, 4);
+    testAdding(list, e1, v, 1);
+    testAdding(list, e2, v, 2);
+    testAdding(list, e3, v, 3);
+    testAdding(list, e4, v, 4);
+    std::vector<int> v2{24, 2, 44, 26, 94};
+    testAddingFront(list, e5, v2, 5);
 }
 
 TEST(AFListTests, canDeleteElements) {
-    af_list::list_head list;
+    af_list<helper> list;
     helper e1(2), e2(44), e3(26), e4(94);
     std::vector<int> v{2, 44, 26, 94};
-    addTest(list, e1, v, 1);
-    addTest(list, e2, v, 2);
-    addTest(list, e3, v, 3);
-    addTest(list, e4, v, 4);
+    testAdding(list, e1, v, 1);
+    testAdding(list, e2, v, 2);
+    testAdding(list, e3, v, 3);
+    testAdding(list, e4, v, 4);
     std::vector<int> v2{2, 44, 94};
-    delTest(list, e3, v2);
+    testDeleting(list, e3, v2);
     std::vector<int> v3{44, 94};
-    delTest(list, e1, v3);
-}
-
-TEST(AFListTests2, canDeleteElements) {
-    af_list::af_list<helper2> list;
-    helper2 e1(2), e2(44), e3(26), e4(94);
-    std::vector<int> v{2, 44, 26, 94};
-    addTest2(list, e1, v, 1);
-    addTest2(list, e2, v, 2);
-    addTest2(list, e3, v, 3);
-    addTest2(list, e4, v, 4);
-    std::vector<int> v2{2, 44, 94};
-    delTest2(list, e3, v2);
-    std::vector<int> v3{44, 94};
-    delTest2(list, e1, v3);
+    testDeleting(list, e1, v3);
 }
 
