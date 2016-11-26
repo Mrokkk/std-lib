31f611ee5b85e1e1ba90d7875195baf4d17a5eb2
Mrokkk <mrokk@protonmail.com>
2016-11-26 13:15:04 +0100

    Work on inherited_list

--- include/inherited_list.h
+++ include/inherited_list.h
@@ -1,24 +1,98 @@
 #pragma once
 
 template <typename Type>
-struct inherited_list {
+class inherited_list {
 
-    Type *prev, *next;
-
-    inherited_list() {
-        prev = reinterpret_cast<Type *>(this);
-        next = reinterpret_cast<Type *>(this);
-    }
+    Type *_prev, *_next;
 
     void add_element(Type *new_element, Type *prev, Type *next) {
-        next->prev = new_element;
-        prev->next = new_element;
-        new_element->next = next;
-        new_element->prev = prev;
+        next->_prev = new_element;
+        prev->_next = new_element;
+        new_element->_next = next;
+        new_element->_prev = prev;
     }
 
-    void add(Type *new_element) {
-        add_element(new_element, prev, reinterpret_cast<Type *>(this));
+public:
+
+    class iterator {
+
+        Type *_ptr = nullptr;
+
+    public:
+
+        iterator(Type *t)
+            : _ptr(t) {}
+
+        iterator &operator++() {
+            _ptr = _ptr->next();
+            return *this;
+        }
+
+        iterator operator++(int) {
+            _ptr = _ptr->next();
+            return *this;
+        }
+
+        iterator &operator--() {
+            _ptr = _ptr->prev();
+            return *this;
+        }
+
+        iterator operator--(int) {
+            _ptr = _ptr->prev();
+            return *this;
+        }
+
+        Type &operator*() {
+            return *_ptr;
+        }
+
+        Type *operator->() {
+            return _ptr;
+        }
+
+        bool operator!=(const iterator &comp) {
+            return _ptr != comp._ptr;
+        }
+
+    };
+
+    inherited_list() {
+        _prev = reinterpret_cast<Type *>(this);
+        _next = reinterpret_cast<Type *>(this);
+    }
+
+    Type &add(Type *new_element) {
+        add_element(new_element, _prev, reinterpret_cast<Type *>(this));
+        return *reinterpret_cast<Type *>(this);
+    }
+
+    Type &remove() {
+        _next->prev = _prev;
+        _prev->next = _next;
+        _next = reinterpret_cast<Type *>(this);
+        _prev = reinterpret_cast<Type *>(this);
+        return *reinterpret_cast<Type *>(this);
+    }
+
+    Type *next() {
+        return _next;
+    }
+
+    Type *prev() {
+        return _prev;
+    }
+
+    bool empty() {
+        return _next == reinterpret_cast<Type *>(this) && _next == reinterpret_cast<Type *>(this);
+    }
+
+    auto begin() {
+        return iterator(_next);
+    }
+
+    auto end() {
+        return iterator(reinterpret_cast<Type *>(this));
     }
 
 };
--- tests/inherited_list.cpp
+++ tests/inherited_list.cpp
@@ -1,32 +1,42 @@
 #include <inherited_list.h>
 #include <gtest/gtest.h>
-#include <iostream>
+#include <vector>
+
+namespace {
 
 struct helper : inherited_list<helper> {
     int a = 0;
     helper(int x) : a(x) {}
 };
 
+void testAdding(inherited_list<helper> &head, helper &e, std::vector<int> &comp, int s) {
+    auto size = 0;
+    head.add(&e);
+    for (const auto &h : head) {
+        EXPECT_EQ(comp[size], h.a);
+        size++;
+    };
+    EXPECT_EQ(size, s);
+    EXPECT_EQ(head.empty(), false);
+}
+
+} // namespace anon
+
 TEST(inherited_list, can_create_empty) {
     helper h(2);
-    ASSERT_EQ(&h, h.prev);
-    ASSERT_EQ(&h, h.next);
+    ASSERT_EQ(&h, h.prev());
+    ASSERT_EQ(&h, h.next());
     ASSERT_EQ(h.a, 2);
+    ASSERT_TRUE(h.empty());
 }
 
 TEST(inherited_list, can_add_elements) {
     inherited_list<helper> head;
     helper h1(2), h2(34), h3(29);
-    head.add(&h1);
-    ASSERT_EQ(head.next->a, 2);
-    ASSERT_EQ(head.next, &h1);
-    ASSERT_EQ(head.prev, &h1);
-    ASSERT_EQ(head.next->next, &head);
-    head.add(&h2);
-    ASSERT_EQ(head.next->a, 2);
-    ASSERT_EQ(head.next->next->a, 34);
-    ASSERT_EQ(head.next, &h1);
-    ASSERT_EQ(head.next->next, &h2);
-    ASSERT_EQ(head.prev, &h2);
+    std::vector<helper> c{helper{2}, helper{3}};
+    std::vector<int> comp{2, 34, 29};
+    testAdding(head, h1, comp, 1);
+    testAdding(head, h2, comp, 2);
+    testAdding(head, h3, comp, 3);
 }
 
