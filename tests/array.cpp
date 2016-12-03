36d1259402cb469cebbf497f709402e46551560f
Mrokkk <mrokk@protonmail.com>
2016-12-03 11:55:34 +0100

    array tests using yatf

--- tests/array.cpp
+++ tests/array.cpp
@@ -3,14 +3,14 @@
 
 using namespace yacppl;
 
-TEST(ArrayTests, canCreateEmptyString) {
+TEST(array, can_be_empty) {
     array<int, 0> array;
     unsigned result = array.size();
     REQUIRE(result == 0);
     REQUIRE(array.end() - array.begin() == 0);
 }
 
-TEST(ArrayTests, canCreateInitializedArray) {
+TEST(array, can_be_initialized) {
     array<int, 5> array{2, 3, 4, 5, 6};
     REQUIRE(array[0] == 2);
     REQUIRE(array[4] == 6);
@@ -18,85 +18,84 @@ TEST(ArrayTests, canCreateInitializedArray) {
     REQUIRE(array.end() - array.begin() == 5);
 }
 
-#if 0
-TEST(ArrayTests, canReadElementsSequentially) {
+TEST(array, can_read_elements_sequentially) {
     array<int, 5> array{2, 3, 4, 5, 6};
-    int cArray[5] = {0, 0, 0, 0, 0};
+    int c_array[5] = {0, 0, 0, 0, 0};
     unsigned i = 0;
     for (const auto &elem : array)
-        cArray[i++] = elem;
-    EXPECT_EQ(cArray[0], 2);
-    EXPECT_EQ(cArray[2], 4);
-    EXPECT_EQ(cArray[4], 6);
+        c_array[i++] = elem;
+    REQUIRE(c_array[0] == 2);
+    REQUIRE(c_array[2] == 4);
+    REQUIRE(c_array[4] == 6);
 }
 
-TEST(ArrayTests, canModifyElementsSequentially) {
+TEST(array, can_modify_elements_sequentially) {
     array<int, 5> array{2, 3, 4, 5, 6};
     unsigned i = 10;
     for (auto &elem : array)
         elem = i++;
-    EXPECT_EQ(array[0], 10);
-    EXPECT_EQ(array[2], 12);
-    EXPECT_EQ(array[4], 14);
+    REQUIRE(array[0] == 10);
+    REQUIRE(array[2] == 12);
+    REQUIRE(array[4] == 14);
 }
 
-TEST(ArrayTests, canModify) {
+TEST(array, can_be_modified_using_brackets) {
     array<int, 5> array{2, 3, 4, 5, 6};
     array[0] = 10;
     array[1] = 11;
     array[4] = 14;
-    EXPECT_EQ(array[0], 10);
-    EXPECT_EQ(array[1], 11);
-    EXPECT_EQ(array[4], 14);
+    REQUIRE(array[0] == 10);
+    REQUIRE(array[1] == 11);
+    REQUIRE(array[4] == 14);
 }
 
-TEST(ArrayTests, canIncrementIterator) {
+TEST(array, can_increment_iterator) {
     array<int, 5> array{2, 3, 4, 5, 6};
     auto it = array.begin();
-    EXPECT_EQ(*it, 2);
+    REQUIRE(*it == 2);
     it++;
-    EXPECT_EQ(*it, 3);
+    REQUIRE(*it == 3);
     ++it;
-    EXPECT_EQ(*it, 4);
+    REQUIRE(*it == 4);
 }
 
-TEST(ArrayTests, canDecrementIterator) {
+TEST(array, can_decrement_iterator) {
     array<int, 5> array{2, 3, 4, 5, 6};
     auto it = array.end();
     it--;
-    EXPECT_EQ(*it, 6);
+    REQUIRE(*it == 6);
     --it;
-    EXPECT_EQ(*it, 5);
+    REQUIRE(*it == 5);
 }
 
-TEST(ArrayTests, canIncrementAndDecrementIterator) {
+TEST(array, can_increment_and_decrement_iterator) {
     array<int, 5> array{2, 3, 4, 5, 6};
     auto it = array.begin();
-    EXPECT_EQ(*it, 2);
+    REQUIRE(*it == 2);
     it++;
-    EXPECT_EQ(*it, 3);
+    REQUIRE(*it == 3);
     it--;
-    EXPECT_EQ(*it, 2);
+    REQUIRE(*it == 2);
 }
 
-TEST(ArrayTests, canCompareIterators) {
+TEST(array, can_compare_iterators) {
     array<int, 5> array{2, 3, 4, 5, 6};
     auto it1 = array.begin();
     auto it2 = array.begin();
-    EXPECT_EQ(it1 == it2, true);
+    REQUIRE(it1 == it2);
     it1++;
-    EXPECT_EQ(it1 != it2, true);
+    REQUIRE(it1 != it2);
 }
 
-TEST(ArrayTests, canBeCopied) {
+TEST(array, can_be_copied) {
     array<int, 5> array{2, 3, 4, 5, 6};
     auto array2 = array;
-    EXPECT_EQ(2, array2[0]);
+    REQUIRE(2 == array2[0]);
     auto it = array2.begin();
-    EXPECT_TRUE(array2.end() - it == 5);
+    REQUIRE(array2.end() - it == 5);
     ++it;
-    EXPECT_TRUE(array2.end() - it == 4);
+    REQUIRE(array2.end() - it == 4);
     it++;
-    EXPECT_TRUE(array2.end() - it == 3);
+    REQUIRE(array2.end() - it == 3);
 }
-#endif
+
