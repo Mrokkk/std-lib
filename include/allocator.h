2f85d10f798eea1a04bff5647082dbdf120ae2cb
Mrokkk <mrokk@protonmail.com>
2016-12-14 22:50:36 +0100

    Minor changes

--- include/allocator.h
+++ include/allocator.h
@@ -5,7 +5,7 @@
 
 namespace yacppl {
 
-template <class Heap_Allocator, size_t _memory_block_size>
+template <class BackendAllocator, size_t _memory_block_size>
 class allocator final {
 
     class memory_block : public inherited_list<memory_block> {
@@ -13,7 +13,7 @@ class allocator final {
         struct _data {
             unsigned char data[_memory_block_size];
             unsigned int block_ptr[0];
-        } __packed;
+        } __attribute__((packed));
 
         template <typename Ptr>
         Ptr pointer_offset(Ptr ptr, int off) {
@@ -40,12 +40,10 @@ class allocator final {
         }
 
         void try_to_divide(size_t pivot) {
-            if (size <= pivot + 2 * _memory_block_size) {
+            if (size <= pivot + 2 * _memory_block_size)
                 free = false;
-            }
-            else {
+            else
                 divide(pivot);
-            }
         }
 
         void *data() {
@@ -59,7 +57,7 @@ class allocator final {
     };
 
     inherited_list<memory_block> _blocks;
-    Heap_Allocator _heap_allocator;
+    BackendAllocator _backend_allocator;
 
     void adapt_size(size_t &size) {
         if (size % _memory_block_size)
@@ -67,13 +65,13 @@ class allocator final {
     }
 
     memory_block *create_memory_block(size_t size) {
-        return new(_heap_allocator.grow_heap(_memory_block_size + size)) memory_block(size);
+        return new(_backend_allocator.grow_heap(_memory_block_size + size)) memory_block(size);
     }
 
 public:
 
     explicit allocator(char *heap_start)
-        : _heap_allocator(heap_start) {}
+        : _backend_allocator(heap_start) {}
 
     void *allocate(size_t size) {
         adapt_size(size);
@@ -84,7 +82,7 @@ public:
             }
         }
         auto new_block = create_memory_block(size);
-        if (new_block == nullptr) return 0;
+        if (new_block == nullptr) return nullptr;
         _blocks.add(new_block);
         return new_block->data();
     }
