8eee9ba50078833f56d908114d44e51c681c41ce
Mrokkk <mrokk@protonmail.com>
2016-12-14 21:50:35 +0100

    Update types.h

--- include/types.h
+++ include/types.h
@@ -2,12 +2,30 @@
 
 namespace yacppl {
 
-template <class T> struct remove_const { typedef T type; };
-template <class T> struct remove_const<const T> { typedef T type; };
+template <typename T, T v>
+struct integral_constant {
+    static constexpr T value = v;
+    using value_type = T;
+    using type = integral_constant;
+    constexpr operator value_type() const { return value; }
+    constexpr value_type operator()() const { return value; }
+};
 
-template <class T> struct remove_reference { typedef T type; };
-template <class T> struct remove_reference<T&> { typedef T type; };
-template <class T> struct remove_reference<T&&> { typedef T type; };
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
+template <typename T> struct remove_const { using type = T; };
+template <typename T> struct remove_const<const T> { using type = T; };
+
+template <typename T> struct remove_reference { using type = T; };
+template <typename T> struct remove_reference<T&> { using type = T; };
+template <typename T> struct remove_reference<T&&> { using type = T; };
+
+template <bool B, typename T = void> struct enable_if {};
+template <typename T> struct enable_if<true, T> { using type = T; };
+
+template <typename T, class U> struct is_same : false_type {};
+template <typename T> struct is_same<T, T> : true_type {};
 
 } // namespace yacppl
 
