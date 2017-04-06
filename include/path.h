03be03fa843dbee1c7c6749bcfcdbc262c543240
Mrokkk <mrokk@protonmail.com>
2017-04-06 16:54:24 +0200

    WIP: path library

--- include/path.h
+++ include/path.h
@@ -1,37 +1,94 @@
 #pragma once
 
+#include <cstring>
+
 namespace yacppl {
 
 class path {
 
-    // TODO: create string class
+    static const constexpr unsigned initial_size_ = 256;
+
     char *path_;
+    char *it_;
+
+    char *remove_trailing_slash(char *start, char *end) {
+        end--;
+        while (end != start) {
+            if (*end != '/') {
+                return end + 1;
+            }
+            else {
+                *end = 0;
+            }
+            end--;
+        }
+        return end;
+    }
 
 public:
 
     path()
-        : path_(new char[256]) {
+            : path_(new char[initial_size_]) {
+        it_ = path_;
+        *it_ = 0;
     }
 
-    path(const char *str)
-            : path_(new char[256]) {
-        // FIXME
-        auto ptr = path_;
-        while (*str) {
-            *ptr = *str++;
-        }
+    ~path() {
+        if (path_)
+            delete path_;
     }
 
-    path &operator/(const char *) {
+    path(const path &p)
+            : path_(new char[initial_size_]) {
+        it_ = path_;
+        append(p.path_);
+    }
+
+    path &operator=(const path &p) {
+        it_ = path_;
+        append(p.path_);
         return *this;
     }
 
-    path &operator/(const path &) {
+    path(const char *str)
+            : path_(new char[initial_size_]) {
+        it_ = path_;
+        append(str);
+    }
+
+    void append(const char *str) {
+        *it_++ = '/';
+        while (*str == '/') {
+            str++;
+        }
+        while (*str) {
+            if (*str == '/' && str[1] == '/') {
+                str++;
+                continue;
+            }
+            *it_++ = *str++;
+        }
+        *it_ = 0;
+        it_ = remove_trailing_slash(path_, it_);
+    }
+
+    path operator/(const char *str) {
+        path p(path_);
+        p.append(str);
+        return p;
+    }
+
+    path &operator/=(const path &p) {
+        append(p.path_);
         return *this;
     }
 
     operator const char*() const {
         return path_;
+    }
+
+    bool operator==(const char *str) const {
+        return strcmp(path_, str) == 0;
     }
 
 };
--- tests/path.cpp
+++ tests/path.cpp
@@ -3,7 +3,22 @@
 
 using namespace yacppl;
 
-TEST(path_tests, can_be_created) {
+TEST(path_tests, empty_path_is_empty_string) {
     path p;
+    REQUIRE(p == "");
+}
+
+TEST(path_tests, path_constructed_from_string_is_valid) {
+    path p("/root/dir");
+    REQUIRE(p == "/root/dir");
+}
+
+TEST(path_tests, can_be_appended) {
+    path p;
+    p = p / "root" / "////test//" / "/dir////";
+    //REQUIRE_EQ(p.operator const char *(), "/root/test/dir");
+    REQUIRE(p == "/root/test/dir");
+    auto p2 = p / path("dir2");
+    REQUIRE(p2 == "/root/test/dir/dir2");
 }
 
