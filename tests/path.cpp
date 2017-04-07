7c185992f2a37f4fc4aabc5a88090f420d5ff591
Mrokkk <mrokk@protonmail.com>
2017-04-07 19:36:47 +0200

    WIP: path

--- tests/path.cpp
+++ tests/path.cpp
@@ -3,20 +3,20 @@
 
 using namespace yacppl;
 
-TEST(path_tests, empty_path_is_empty_string) {
+TEST(path, empty_path_is_empty_string) {
     path p;
     REQUIRE(p == "");
     REQUIRE_EQ(p.get(), "");
 }
 
-TEST(path_tests, path_constructed_from_string_is_valid) {
+TEST(path, path_constructed_from_string_is_valid) {
     path p("/root/dir");
     REQUIRE(p == "/root/dir");
     path p2("some_dir/another_dir");
     REQUIRE(p2 == "some_dir/another_dir");
 }
 
-TEST(path_tests, paths_can_be_appended) {
+TEST(path, paths_can_be_appended) {
     path p1("home");
     path p2("user");
     path p3(p1 / p2);
@@ -30,7 +30,7 @@ TEST(path_tests, paths_can_be_appended) {
     REQUIRE(p4 == "home/user/dir/next_dir/test");
 }
 
-TEST(path_tests, paths_are_normalized) {
+TEST(path, paths_are_normalized) {
     path p;
     p = p / "root" / "////test//" / "/dir////";
     REQUIRE(p == "/root/test/dir");
@@ -42,10 +42,20 @@ TEST(path_tests, paths_are_normalized) {
     REQUIRE(p3 == "/dir");
 }
 
-TEST(path_tests, can_get_basename) {
+TEST(path, can_get_basename) {
     path p("/root/dir1/dir2//dir3/");
     REQUIRE_EQ(p.basename(), "dir3");
     path p2("some_dir");
     REQUIRE_EQ(p2.basename(), "some_dir");
+}
+
+TEST(path, can_be_iterated) {
+    path p("/root/dir");
+    auto it = p.cbegin();
+    REQUIRE_EQ((const char *)*it, "root")
+    ++it;
+    REQUIRE_EQ((const char *)*it, "dir")
+    path p2("root/dir");
+    REQUIRE_EQ((const char *)*p2.cbegin(), "root")
 }
 
