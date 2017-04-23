409e9853c7cc7044e15cd6aeeaaca04d2634f50e
Mrokkk <mrokk@protonmail.com>
2017-04-23 17:07:17 +0200

    Fixing iterators in inherited_list

--- include/inherited_list.h
+++ include/inherited_list.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "types.h"
+
 namespace yacppl {
 
 template <typename Type>
@@ -18,52 +20,66 @@ class inherited_list {
         return *reinterpret_cast<Type *>(this);
     }
 
-public:
+    template <bool is_const>
+    struct detail_iterator {
 
-    class iterator {
+        using value_type = Type;
+        using reference = typename conditional<is_const, const value_type &, value_type &>::type;
+        using pointer = typename conditional<is_const, const value_type *, value_type *>::type;
+
+    private:
 
         Type *ptr_ = nullptr;
 
     public:
 
-        constexpr iterator(Type *t)
+        constexpr detail_iterator(Type *t)
             : ptr_(t) {}
 
-        iterator &operator++() {
+        detail_iterator &operator++() {
             ptr_ = ptr_->next();
             return *this;
         }
 
-        iterator operator++(int) {
+        detail_iterator operator++(int) {
             auto tmp = *this;
             ptr_ = ptr_->next();
             return tmp;
         }
 
-        iterator &operator--() {
+        detail_iterator &operator--() {
             ptr_ = ptr_->prev();
             return *this;
         }
 
-        iterator operator--(int) {
+        detail_iterator operator--(int) {
             auto tmp = *this;
             ptr_ = ptr_->prev();
             return tmp;
         }
 
-        Type &operator*() {
+        reference operator*() {
             return *ptr_;
         }
 
-        Type *operator->() {
+        pointer operator->() {
             return ptr_;
         }
 
-        bool operator!=(const iterator &comp) {
+        bool operator==(const detail_iterator &comp) const {
+            return ptr_ == comp.ptr_;
+        }
+
+        bool operator!=(const detail_iterator &comp) const {
             return ptr_ != comp.ptr_;
         }
 
     };
+
+public:
+
+    using iterator = detail_iterator<false>;
+    using const_iterator = detail_iterator<true>;
 
     constexpr inherited_list() {
         next_ = prev_ = reinterpret_cast<Type *>(this);
