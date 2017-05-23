932fa567c7ef9d9cf72ee5bcbce88aa15be3d404
Mrokkk <mrokk@protonmail.com>
2017-05-23 22:21:51 +0200

    Refactoring; add thread_safe/shared_ptr.cpp

--- tests/thread_safe/main.cpp
+++ tests/thread_safe/main.cpp
@@ -1,7 +1,7 @@
-#include <shared_ptr.hpp>
-#include <thread>
-
-yacppl::shared_ptr<int> ptr;
+#define YATF_MAIN
+#include <cstdio>
+#include <yatf.hpp>
+#include <spinlock.hpp>
 
 void atomic_increment(void *addr) {
     asm volatile("lock incl (%0)" :: "r" (addr));
@@ -31,43 +31,7 @@ void spinlock_unlock(volatile size_t *lock) {
         : "memory");
 }
 
-void thread1() {
-    for (auto i = 0u; i < 1024u; ++i) {
-        auto ptr1 = ptr;
-    }
-    ptr = nullptr;
-    for (auto i = 0u; i < 1024u; ++i) {
-        auto ptr1 = ptr;
-    }
-    ptr = nullptr;
-    for (auto i = 0u; i < 1024u; ++i) {
-        auto ptr1 = yacppl::make_shared<int>(323);
-        ptr = ptr1;
-        ptr1 = nullptr;
-    }
-}
-
-void thread2() {
-    for (auto i = 0u; i < 2048u; ++i) {
-        auto ptr1 = ptr;
-    }
-    ptr = new int(4);
-    for (auto i = 0u; i < 2048u; ++i) {
-        auto ptr1 = ptr;
-    }
-    ptr = nullptr;
-}
-
-int main() {
-    ptr = yacppl::make_shared(2);
-    std::thread t1(thread1);
-    std::thread t2(thread2);
-    std::thread t3(thread2);
-    std::thread t4(thread1);
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    return 0;
+int main(int argc, const char *argv[]) {
+    return yatf::main(printf, argc, argv);
 }
 
---
+++ tests/thread_safe/shared_ptr.cpp
@@ -0,0 +1,47 @@
+#include <thread>
+#include <yatf.hpp>
+#include <shared_ptr.hpp>
+
+yacppl::shared_ptr<int> ptr;
+
+void thread1() {
+    for (auto i = 0u; i < 1024u; ++i) {
+        auto ptr1 = ptr;
+    }
+    ptr = nullptr;
+    for (auto i = 0u; i < 1024u; ++i) {
+        auto ptr1 = ptr;
+    }
+    ptr = nullptr;
+    for (auto i = 0u; i < 1024u; ++i) {
+        auto ptr1 = yacppl::make_shared<int>(323);
+        ptr = ptr1;
+        ptr1 = nullptr;
+    }
+}
+
+void thread2() {
+    for (auto i = 0u; i < 2048u; ++i) {
+        auto ptr1 = ptr;
+    }
+    ptr = new int(4);
+    for (auto i = 0u; i < 2048u; ++i) {
+        auto ptr1 = ptr;
+    }
+    ptr = nullptr;
+}
+
+TEST(shared_ptr, is_thread_safe) {
+    for (auto i = 0; i < 1024; ++i) {
+        ptr = yacppl::make_shared(2);
+        std::thread t1(thread1);
+        std::thread t2(thread2);
+        std::thread t3(thread2);
+        std::thread t4(thread1);
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+    }
+}
+
