7e1f65d78dcb73a955fe67e39d2955ea1a49fd9f
Mrokkk <mrokk@protonmail.com>
2017-06-15 18:37:21 +0200

    Add tuple

---
+++ include/tuple.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <cstddef>
+
+namespace yacppl {
+
+namespace detail {
+
+template <size_t ...N>
+struct expand {
+    using type = expand<N...>;
+};
+
+template <size_t N, typename ...T>
+struct choose_nth {
+};
+
+template <size_t N, typename T, typename ...U>
+struct choose_nth<N, T, U...> : choose_nth<N - 1, U...> {
+};
+
+template <typename T, typename ...U>
+struct choose_nth<0, T, U...> {
+    using type = T;
+};
+
+template <size_t N, typename T>
+class tuple_element {
+    const T value_;
+public:
+    explicit tuple_element(const T &val) : value_(val) {
+    }
+    const T &get() const {
+        return value_;
+    }
+};
+
+template <size_t L, size_t I = 0, typename S = expand<>>
+struct range {
+};
+
+template <size_t L, size_t I, size_t ...N>
+struct range<L, I, expand<N...>> : range<L, I + 1, expand<N..., I>> {
+};
+
+template <size_t L, size_t ...N>
+struct range<L, L, expand<N...>> : expand<N...> {
+};
+
+template <typename N, typename ...T>
+class tuple_impl {
+};
+
+template <size_t ...N, typename ...T>
+struct tuple_impl<expand<N...>, T...> : public tuple_element<N, T>... {
+
+    template <size_t M> using value_type = typename choose_nth<M, T...>::type;
+
+    explicit tuple_impl(const T &...values) : tuple_element<N, T>(values)... {
+    }
+
+    template <size_t M>
+    const value_type<M> &get() const {
+        return tuple_element<M, value_type<M>>::get();
+    }
+
+};
+
+} // namespace detail
+
+template <typename ...T>
+struct tuple : public detail::tuple_impl<typename detail::range<sizeof...(T)>::type, T...> {
+
+    explicit tuple(const T &...values)
+        : detail::tuple_impl<typename detail::range<sizeof...(T)>::type, T...>(values...) {
+    }
+
+    static constexpr std::size_t size() {
+        return sizeof...(T);
+    }
+
+};
+
+} // namespace yacppl
+
---
+++ tests/tuple.cpp
@@ -0,0 +1,26 @@
+#include <tuple.hpp>
+#include "yatf/include/yatf.hpp"
+
+using namespace yacppl;
+
+TEST(tuple, can_be_created) {
+    {
+        tuple<int> t(-923);
+        REQUIRE_EQ(t.get<0>(), -923);
+        REQUIRE_EQ(t.size(), 1u);
+    }
+    {
+        tuple<int, char> t(2, 'c');
+        REQUIRE_EQ(t.get<0>(), 2);
+        REQUIRE_EQ(t.get<1>(), 'c');
+        REQUIRE_EQ(t.size(), 2u);
+    }
+    {
+        tuple<int, char, unsigned int> t(9835, 'c', 123);
+        REQUIRE_EQ(t.get<0>(), 9835);
+        REQUIRE_EQ(t.get<1>(), 'c');
+        REQUIRE_EQ(t.get<2>(), 123u);
+        REQUIRE_EQ(t.size(), 3u);
+    }
+}
+
